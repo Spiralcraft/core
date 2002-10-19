cde49e941e065bee9be3bd75007b9f93876424d7
mike <>
2002-10-19 02:36:53 +0000

    Latest

---
+++ source/spiralcraft/sax/ParseTree.java
@@ -0,0 +1,14 @@
+package spiralcraft.sax;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * A lightweight parse tree of an XML document which captures and preserves SAX events
+ */
+public class ParseTree
+  extends DefaultHandler
+{
+  
+
+
+}
