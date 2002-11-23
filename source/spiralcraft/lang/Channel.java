ce1cf0001d446c5cf2f16940974519c073648c2c
mike <>
2002-11-23 03:04:12 +0000

    Store expression

--- source/spiralcraft/lang/Channel.java
+++ source/spiralcraft/lang/Channel.java
@@ -5,8 +5,15 @@ import spiralcraft.lang.optics.ProxyOptic;
 public class Channel
   extends ProxyOptic
 {
-  public Channel(Optic optic)
-  { super(optic);
+  private Expression _expression;
+
+  public Channel(Optic optic,Expression expression)
+  { 
+    super(optic);
+    _expression=expression;
   }
-  
+
+  public Expression getExpression()
+  { return _expression;
+  }
 }
