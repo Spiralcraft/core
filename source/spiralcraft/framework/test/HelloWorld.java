d01b24552df7d82bb5e076d42361b31b5071b6e7
mike <>
2002-11-23 03:05:06 +0000

    Working test

--- source/spiralcraft/framework/test/HelloWorld.java
+++ source/spiralcraft/framework/test/HelloWorld.java
@@ -7,10 +7,20 @@ import spiralcraft.framework.Executable;
 public class HelloWorld
   implements Executable
 {
+  private Object _testObject;
 
   public void exec(String[] args)
   { 
     System.out.println("HelloWorld");
     System.out.println("args: "+ArrayUtil.formatToString(args," ","\""));
+    System.out.println("testObject:");
+    
+    if (_testObject!=null)
+    { System.out.println(_testObject.toString());
+    }
+  }
+
+  public void setTestObject(Object testObject)
+  { _testObject=testObject;
   }
 }
