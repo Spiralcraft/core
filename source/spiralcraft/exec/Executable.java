8d8500d4b597bd35311a49cf154c079c5599d67e
mike <>
2002-12-01 21:33:29 +0000

    Exec package

---
+++ source/spiralcraft/exec/Executable.java
@@ -0,0 +1,20 @@
+package spiralcraft.exec;
+
+/**
+ * Supplies an entry point for application execution.
+ *
+ * Implemented by the root object of the runtime object hierarchy
+ *   for an application instance.
+ *
+ * An Executable is normally instantiated from an Assembly (spiralcraft.builder) and
+ *   invoked by the Executor.
+ */
+public interface Executable
+{
+  
+  /**
+   * Execute the application
+   */
+  public void exec(String[] args);
+
+}
---
+++ source/spiralcraft/exec/test/HelloWorld.java
@@ -0,0 +1,26 @@
+package spiralcraft.exec.test;
+
+import spiralcraft.util.ArrayUtil;
+
+import spiralcraft.exec.Executable;
+
+public class HelloWorld
+  implements Executable
+{
+  private Object _testObject;
+
+  public void exec(String[] args)
+  { 
+    System.out.println("HelloWorld");
+    System.out.println("args: "+ArrayUtil.formatToString(args," ","\""));
+    System.out.println("testObject:");
+    
+    if (_testObject!=null)
+    { System.out.println(_testObject.toString());
+    }
+  }
+
+  public void setTestObject(Object testObject)
+  { _testObject=testObject;
+  }
+}
