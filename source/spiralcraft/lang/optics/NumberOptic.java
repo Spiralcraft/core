1e898c532d466645b818616918cf31a2869ccab6
mike <>
2002-12-19 02:55:32 +0000

    Divide operation, refactor

--- source/spiralcraft/lang/optics/NumberOptic.java
+++ source/spiralcraft/lang/optics/NumberOptic.java
@@ -2,15 +2,51 @@ package spiralcraft.lang.optics;
 
 import spiralcraft.lang.Optic;
 import spiralcraft.lang.Expression;
+import spiralcraft.lang.Focus;
+import spiralcraft.lang.BindException;
+
+import java.math.BigDecimal;
 
 /**
  * An abstract Optic for representing a Number
  */
-public abstract class NumberOptic
-  extends AbstractOptic
+public class NumberOptic
+  extends BeanOptic
 {
-  
-  public Class getTargetClass()
-  { return Number.class;
+  public NumberOptic(Optic source)
+    throws BindException
+  { super(source);
   }
+  
+  public Optic resolve(Focus focus,String name,Expression[] params)
+    throws BindException
+  {
+    if (name.equals("/"))
+    { return new NumberDivideOptic(this,params[0].createChannel(focus));
+    }
+    return super.resolve(focus,name,params);
+  }
+}
+
+class NumberDivideOptic
+  extends NumberOptic
+{
+  private final Optic _divisor;
+
+  public NumberDivideOptic(Optic source,Optic divisor)
+    throws BindException
+  { 
+    super(source);
+    _divisor=divisor;
+  }
+
+  public Object get()
+  { 
+    Number source=(Number) super.get();
+    Number divisor=(Number) _divisor.get();
+    return new Double(source.doubleValue()/divisor.doubleValue());
+  }
+
+  
+
 }
