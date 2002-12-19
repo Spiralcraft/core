f5bed92ba3cbc681b38f16ac506b5b5609f0d38a
mike <>
2002-12-19 03:02:03 +0000

    Support fields

--- source/spiralcraft/beans/MappedBeanInfo.java
+++ source/spiralcraft/beans/MappedBeanInfo.java
@@ -5,22 +5,26 @@ import java.util.HashMap;
 import java.beans.PropertyDescriptor;
 import java.beans.BeanInfo;
 
+import java.lang.reflect.Field;
+
 public class MappedBeanInfo
   extends BeanInfoProxy
 {
   private HashMap _propertyMap;
+  private HashMap _fieldMap;
+  private Field[] _fields;
 
   public MappedBeanInfo(BeanInfo binf)
   { 
     super(binf);
     mapProperties();
+    mapFields();
   }
 
   private void mapProperties()
   {
     PropertyDescriptor[] props
       =beanInfo.getPropertyDescriptors();
-
     _propertyMap=new HashMap(props.length);
 
     for (int i=0;i<props.length;i++)
@@ -28,8 +32,21 @@ public class MappedBeanInfo
     }
   }
 
+  private void mapFields()
+  {
+    _fields=getBeanDescriptor().getBeanClass().getFields();
+    _fieldMap=new HashMap(_fields.length);
+
+    for (int i=0;i<_fields.length;i++)
+    { _fieldMap.put(_fields[i].getName(),_fields[i]);
+    }
+  }
+
   public PropertyDescriptor findProperty(String name)
   { return (PropertyDescriptor) _propertyMap.get(name);
   }
 
+  public Field findField(String name)
+  { return (Field) _fieldMap.get(name);
+  }
 }
