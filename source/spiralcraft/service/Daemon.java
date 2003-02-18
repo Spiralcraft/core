8e5137f83a8c7b72c2aa2b269e484c091d8caa44
mike <>
2003-02-18 06:58:04 +0000

    Logging, standard arguments

--- source/spiralcraft/service/Daemon.java
+++ source/spiralcraft/service/Daemon.java
@@ -1,5 +1,7 @@
 package spiralcraft.service;
 
+import spiralcraft.util.Arguments;
+
 import spiralcraft.exec.Executable;
 
 import spiralcraft.ui.Command;
@@ -11,6 +13,7 @@ import spiralcraft.registry.RegistryNode;
 import java.util.logging.Handler;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import spiralcraft.log.RegistryLogger;
 import spiralcraft.log.DefaultFormatter;
@@ -30,7 +33,9 @@ public class Daemon
   private String[] _args;
 
   private Handler _logHandler=new ConsoleHandler();
-  { _logHandler.setFormatter(new DefaultFormatter());
+  { 
+    _logHandler.setFormatter(new DefaultFormatter());
+    _logHandler.setLevel(Level.ALL);
   }
 
   private Logger _logger;
@@ -64,11 +69,34 @@ public class Daemon
   { return _args;
   }
 
+  protected void processArguments()
+  {
+    new Arguments()
+    {
+      protected boolean processOption(String option)
+      { 
+        if (option=="verbose")
+        { _logger.setLevel(Level.FINE);
+        }
+        else
+        { return super.processOption(option);
+        }
+        return true;
+      }
+
+      protected boolean processArgument(String argument)
+      { return super.processArgument(argument);
+      }
+
+    }.process(_args,'-');
+  }
+
   public void execute(String[] args)
   {
     try
     { 
       _args=args;
+      processArguments();
       init(this);
       handleEvents();
       destroy();
