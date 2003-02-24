be58a98200b7e100d86e75db49ada3eb444c33b9
mike <>
2003-02-24 22:35:21 +0000

    Request handling architecture

---
+++ source/spiralcraft/util/Property.java
@@ -0,0 +1,30 @@
+package spiralcraft.util;
+
+/**
+ * Associates a String name with a value
+ */
+public class Property
+{
+  private String _name;
+  private Object _value;
+
+  public Property()
+  {
+  }
+
+  public void setName(String name)
+  { _name=name;
+  }
+
+  public void setValue(Object value)
+  { _value=value;
+  }
+
+  public String getName()
+  { return _name;
+  }
+
+  public Object getValue()
+  { return _value;
+  }
+}
