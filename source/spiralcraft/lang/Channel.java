c4f1c23a4760af1c930ea14b0cbc9fa65c71f209
mike <>
2003-05-22 23:30:52 +0000

    Channel implements Focus now

--- source/spiralcraft/lang/Channel.java
+++ source/spiralcraft/lang/Channel.java
@@ -2,18 +2,58 @@ package spiralcraft.lang;
 
 import spiralcraft.lang.optics.ProxyOptic;
 
+import java.util.HashMap;
+
 public class Channel
   extends ProxyOptic
+  implements Focus
 {
-  private Expression _expression;
+  private final Expression _expression;
+  private final Focus _source;
+  private HashMap _channels;
 
-  public Channel(Optic optic,Expression expression)
+  public Channel(Focus source,Optic optic,Expression expression)
   { 
     super(optic);
     _expression=expression;
+    _source=source;
   }
 
   public Expression getExpression()
   { return _expression;
+  }
+  
+  public Optic getSubject()
+  { return this;
+  }
+  
+  public Focus getParentFocus()
+  { return _source;
+  }
+  
+  public Focus findFocus(String name)
+  { return _source.findFocus(name);
+  }
+  
+  public Environment getEnvironment()
+  { return _source.getEnvironment();
+  }
+  
+  public synchronized Channel bind(Expression expression)
+    throws BindException
+  { 
+    Channel channel=null;
+    if (_channels==null)
+    { _channels=new HashMap();
+    }
+    else
+    { channel=(Channel) _channels.get(expression);
+    }
+    if (channel==null)
+    { 
+      channel=expression.bind(this);
+      _channels.put(expression,channel);
+    }
+    return channel;
   }
 }
