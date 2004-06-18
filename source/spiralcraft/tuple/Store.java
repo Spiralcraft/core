d27a4cf037598c60280169497920e5d8a18b89ce
mike <>
2004-06-18 21:29:36 +0000

    Abstraction of a tuple Store

---
+++ source/spiralcraft/tuple/Store.java
@@ -0,0 +1,11 @@
+package spiralcraft.tuple;
+
+/**
+ * Provides access to a collection of data through the Tuple interface
+ *   and other related interfaces
+ */
+public interface Store
+{
+  
+  
+}
