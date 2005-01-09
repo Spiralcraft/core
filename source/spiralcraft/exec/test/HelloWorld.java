3d8d38909fcd9af29269d027903be94431868fdf
mike <>
2005-01-09 02:27:40 +0000

    ExecutionContext

--- source/spiralcraft/exec/test/HelloWorld.java
+++ source/spiralcraft/exec/test/HelloWorld.java
@@ -3,20 +3,21 @@ package spiralcraft.exec.test;
 import spiralcraft.util.ArrayUtil;
 
 import spiralcraft.exec.Executable;
+import spiralcraft.exec.ExecutionContext;
 
 public class HelloWorld
   implements Executable
 {
   private Object _testObject;
 
-  public void execute(String[] args)
+  public void execute(ExecutionContext context,String[] args)
   { 
-    System.out.println("HelloWorld");
-    System.out.println("args: "+ArrayUtil.formatToString(args," ","\""));
-    System.out.println("testObject:");
+    context.out().println("HelloWorld");
+    context.out().println("args: "+ArrayUtil.formatToString(args," ","\""));
+    context.out().println("testObject:");
     
     if (_testObject!=null)
-    { System.out.println(_testObject.toString());
+    { context.out().println(_testObject.toString());
     }
   }
 
