e53e3c8f36f3726c56849d63af5ec2cf38d58aac
mike <>
2005-02-27 19:54:19 +0000

    Removed obsolete prism instances

--- source/spiralcraft/lang/OpticFactory.java
+++ source/spiralcraft/lang/OpticFactory.java
@@ -6,9 +6,6 @@ import spiralcraft.lang.optics.SimpleBinding;
 import spiralcraft.lang.optics.Binding;
 import spiralcraft.lang.optics.Prism;
 import spiralcraft.lang.optics.BeanPrism;
-import spiralcraft.lang.optics.StringPrism;
-import spiralcraft.lang.optics.NumberPrism;
-import spiralcraft.lang.optics.BigDecimalPrism;
 import spiralcraft.lang.optics.VoidPrism;
 
 import java.beans.IntrospectionException;
@@ -26,18 +23,8 @@ public class OpticFactory
   { return _INSTANCE;
   }
 
-  private final Prism _stringPrism=new StringPrism();
-  private final Prism _numberPrism=new NumberPrism();
-  private final Prism _bigDecimalPrism=new BigDecimalPrism();
   private final HashMap _prismMap
     =new HashMap();
-
-  public OpticFactory()
-  {
-    _prismMap.put(BigDecimal.class,_bigDecimalPrism);
-    _prismMap.put(double.class,_numberPrism);
-    _prismMap.put(String.class,_stringPrism);
-  }
 
   /**
    * Create an Optic which provides view of an arbitrary Java object.
