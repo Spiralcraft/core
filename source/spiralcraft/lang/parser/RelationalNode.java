247f16abf09f4aecd37fd1662f29b6d1deedbeb5
mike <>
2005-02-27 19:38:41 +0000

    Relational operator based on comparable interface

--- source/spiralcraft/lang/parser/RelationalNode.java
+++ source/spiralcraft/lang/parser/RelationalNode.java
@@ -1,33 +1,84 @@
 package spiralcraft.lang.parser;
 
+import spiralcraft.lang.Optic;
 
 public class RelationalNode
-  extends Node
+  extends LogicalNode
 {
 
   private final boolean _greaterThan;
   private final boolean _equals;
-  private final Node _op1;
-  private final Node _op2;
 
   public RelationalNode(boolean greaterThan,boolean equals,Node op1,Node op2)
   { 
+    super(op1,op2);
     _greaterThan=greaterThan;
     _equals=equals;
-    _op1=op1;
-    _op2=op2;
   }
 
-  public void dumpTree(StringBuffer out,String prefix)
+  public Object translateForGet(Object val,Optic[] mods)
   { 
-    out.append(prefix).append("Relational");
-    prefix=prefix+"  ";
-    _op1.dumpTree(out,prefix);
-    out.append(prefix).append(_greaterThan?">":"<");
-    if (_equals)
-    { out.append("=");
+    Comparable val1=(Comparable) val;
+    Comparable val2=(Comparable) mods[0].get();
+    
+    if (val1==null)
+    { 
+      if (val2==null && _equals)
+      { return true;
+      }
+      else
+      { return null;
+      }
     }
-    _op2.dumpTree(out,prefix);
+    
+    int result=val1.compareTo(val2);
+    
+    if (_greaterThan)
+    {
+      if (_equals)
+      { return result>=0;
+      }
+      else
+      { return result>0;
+      }
+    }
+    else
+    {
+      if (_equals)
+      { return result<=0;
+      }
+      else
+      { return result<0;
+      }
+    }
+  }
+  
+  public Object translateForSet(Object val,Optic[] mods)
+  { 
+    // Not reversible
+    throw new UnsupportedOperationException();
+  }
+  
+  public String getSymbol()
+  { 
+    if (_greaterThan)
+    { 
+      if (_equals)
+      { return ">=";
+      }
+      else
+      { return ">";
+      }
+    }
+    else
+    {
+      if (_equals)
+      { return "<=";
+      }
+      else
+      { return "<";
+      }
+    }
   }
 
 }
