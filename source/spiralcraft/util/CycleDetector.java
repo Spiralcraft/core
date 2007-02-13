91c34a4130aeee41c4c4a121b5804ee3ef11cf1f
mike <>
2007-02-13 00:24:55 +0000

    CycleDetector detects cycles in Object traversal using .equals(), hashCode()

---
+++ source/spiralcraft/util/CycleDetector.java
@@ -0,0 +1,54 @@
+//
+// Copyright (c) 1998,2005 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.util;
+
+import java.util.Stack;
+import java.util.HashMap;
+
+/**
+ * Detects cycles using hashCode() and equals()
+ */
+public class CycleDetector
+{
+  private final Stack stack=new Stack();
+  private final HashMap map=new HashMap();
+
+  /**
+   * Detects a duplicate object in the stack. If there is no duplicate
+   *   returns false and pushes the object into the stack. If there is
+   *   a duplicate, returns true.
+   */
+  public boolean detectOrPush(Object object)
+  { 
+    if (map.get(object)!=null)
+    { return true;
+    }
+    stack.push(object);
+    map.put(object,object);    
+    // System.err.println(this.toString()+":"+object.toString());
+    return false;
+  }
+
+  /**
+   * Pops the last element in the stack
+   *
+   * This should be called inside a 'finally' block to prevent leaks.
+   */
+  public void pop()
+  { 
+    map.remove(stack.pop());
+  }
+  
+}
