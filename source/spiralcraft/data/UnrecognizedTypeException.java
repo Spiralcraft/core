bceb86a901a9c72d16b2626885faffc3230b6ecd
mike <>
2007-06-18 11:05:31 +0000

    Exception when a Queryable doesn't know about a requested Type

---
+++ source/spiralcraft/data/UnrecognizedTypeException.java
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 1998,2007 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.data;
+
+/**
+ * Throws when a Type is not found by the TypeResolver
+ */
+public class UnrecognizedTypeException
+  extends DataException
+{
+  private static final long serialVersionUID=1;	
+
+  public UnrecognizedTypeException(Type type,String message)
+  { super("Unrecognized Type : "+type.getURI().toString()+": "+message);
+  }
+}
