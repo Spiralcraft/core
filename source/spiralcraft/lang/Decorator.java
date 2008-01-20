052ea615a02fba06c1c4808d48b189cb70292c2a
mike <>
2008-01-20 22:48:20 +0000

    Docs

--- source/spiralcraft/lang/Decorator.java
+++ source/spiralcraft/lang/Decorator.java
@@ -15,7 +15,30 @@
 package spiralcraft.lang;
 
 /**
- * Base interface for functionality extensions applied to Optics. 
+ * <p>Base interface for functionality extensions applied to Channels</p>
+ * 
+ * <p>A Decorator is bound to a Channel and allows the binding of more 
+ *   specific functionality (for example, Iteration) to the output of a
+ *   channel. The Channel determines which implementation, if any, it
+ *   should use to support a given Decorator interface.
+ * </p> 
+ * 
+ * <p>A Decorator is requested from a Channel by an application component
+ *   which uses the spiralcraft.lang package and expects a specific kind 
+ *   of interface or other functionality to be supported by the object
+ *   referenced by the Channel. 
+ * </p>
+ * 
+ * <p>
+ *   For example, an application component which provides a means for
+ *   iterating through a container referenced by a Channel will request
+ *   an IterationDecorator from the Channel.decorate() method. The
+ *   Channel will determine, usually by querying its associated Reflector,
+ *   which specific implementation of the IterationDecorator it will
+ *   create to satisfy the contract, if any.   
+ *   
+ * </p>
+ * 
  */ 
 public interface Decorator<T>
 {
