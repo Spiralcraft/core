965a1bae210d1ab5678087dc6b4a429544a330ff
mike <>
2008-04-21 23:58:58 +0000

    Type-specific string translation

---
+++ source/spiralcraft/data/util/TypeStringConverter.java
@@ -0,0 +1,33 @@
+package spiralcraft.data.util;
+
+import spiralcraft.data.DataException;
+import spiralcraft.data.Type;
+import spiralcraft.util.StringConverter;
+
+public class TypeStringConverter<T>
+  extends StringConverter<T>
+{
+
+  private Type<T> type;
+  
+  public TypeStringConverter(Type<T> type)
+  { this.type=type;
+  }
+  
+  @Override
+  public T fromString(
+    String val)
+  {
+    try
+    { return type.fromString(val);
+    }
+    catch (DataException x)
+    { throw new IllegalArgumentException(val,x);
+    }
+  }
+  
+  public String toString(T val)
+  { return type.toString(val);
+  }
+
+}
