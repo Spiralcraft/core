28b23c470807f114c2d1ed6f4accd3bb8d09bed4
mike <>
2008-10-15 01:28:25 +0000

    General purpose ParserContext objects to assist in building parsers that track token positions for error reporting and back-linkage purposes

---
+++ source/spiralcraft/text/LookaheadParserContext.java
@@ -0,0 +1,85 @@
+//
+// Copyright (c) 1998,2005 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.text;
+
+import java.io.Reader;
+import java.io.IOException;
+
+import spiralcraft.text.io.CharSequenceReader;
+
+
+/**
+ * <p>Tracks the stream position of a parse operation while exposing data
+ *   via a lookahead interface.
+ * </p>
+ */
+public class LookaheadParserContext
+{
+  private char _currentChar;
+  private boolean _eof=false;
+  private Reader _reader;
+  private ParsePosition position=new ParsePosition();
+
+  public LookaheadParserContext(Reader reader)
+    throws ParseException
+  { 
+    _reader=reader;
+    advance();
+  }
+
+  public LookaheadParserContext(CharSequence text)
+    throws ParseException
+  { 
+    _reader=new CharSequenceReader(text);
+    advance();
+  }
+
+  public ParsePosition getPosition()
+  { return position;
+  }
+  
+  public final char getCurrentChar()
+  { return _currentChar;
+  }
+
+  public final void advance()
+    throws ParseException
+  { 
+    if (_eof)
+    { throw new ParseException("Unexpected end of input",position);
+    }
+    try
+    {
+      
+      int chr=_reader.read();
+      position.incIndex(1);
+      if (chr==-1)
+      { _eof=true;
+      }
+      else
+      { _currentChar=(char) chr;
+      }
+    }
+    catch (IOException x)
+    { throw new ParseException("",position,x);
+    }
+  }
+
+  public final boolean isEof()
+  { return _eof;
+  }
+
+  
+}
---
+++ source/spiralcraft/text/PushbackParserContext.java
@@ -0,0 +1,97 @@
+//
+// Copyright (c) 1998,2008 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.text;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+
+import spiralcraft.text.io.CharSequenceReader;
+
+/**
+ * <p>Tracks the stream position of a parse operation while exposing data
+ *   via a pushback interface.
+ * </p>
+ * 
+ * @author mike
+ *
+ */
+public class PushbackParserContext
+{
+  private boolean eof;
+  private PushbackReader _reader;
+  private ParsePosition position=new ParsePosition();
+
+  public PushbackParserContext(PushbackReader reader)
+    throws ParseException
+  { this._reader=reader;
+  }
+
+  public PushbackParserContext(CharSequence text)
+    throws ParseException
+  { this._reader=new PushbackReader(new CharSequenceReader(text));
+  }
+
+  public ParsePosition getPosition()
+  { return position;
+  }
+  
+  public void throwParseException(String message,String context)
+    throws ParseException
+  { 
+    position.setContext(context);
+    throw new ParseException(message,position);
+  }
+  
+  public void expect(char chr,String context)
+    throws ParseException
+  { 
+    if (read()!=chr)
+    { throwParseException("Expected '"+chr+"'",context);
+    }
+  }
+  
+  public final int read()
+    throws ParseException
+  { 
+    if (eof)
+    { throw new ParseException("Unexpected end of input",position);
+    }
+    try
+    {
+      position.incIndex(1);
+      int ret=_reader.read();
+      if (ret==-1)
+      { eof=true;
+      }
+      return ret;
+    }
+    catch (IOException x)
+    { throw new ParseException("Caught IOException while parsing",position,x);
+    }    
+  }
+
+  public final void unread(char chr)
+    throws ParseException
+  { 
+    position.incIndex(-1);
+    try
+    { _reader.unread(chr);
+    }
+    catch (IOException x)
+    { throw new ParseException("Caught IOException while parsing",position,x);
+    }    
+  }
+
+}
