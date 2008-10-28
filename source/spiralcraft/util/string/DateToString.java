cba3124d34c6902bf9cffeeb026a401c5fb72438
mike <>
2008-10-28 23:13:01 +0000

    Convert between Dates and Strings according to a specified format

---
+++ source/spiralcraft/util/string/DateToString.java
@@ -0,0 +1,103 @@
+//
+// Copyright (c) 1998,2008 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.util.string;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import java.text.ParseException;
+
+/**
+ * <p>A StringConverter for Date objects which uses a 
+ *   java.text.SimpleDateFormat and an associated format string to perform
+ *   the conversion.
+ * </p>
+ * 
+ * <p>The default format string is yyyy-MM-dd HH:mm:ss.S Z
+ * </p>
+ * @author mike
+ *
+ */
+public final class DateToString
+  extends StringConverter<Date>
+{
+  
+  private String formatString="yyyy-MM-dd HH:mm:ss.S Z";
+  
+  private volatile ThreadLocal<SimpleDateFormat> formatLocal;
+  
+  
+  
+
+  public DateToString(String formatString)
+  { setFormatString(formatString);
+  }
+  
+  public DateToString()
+  { newFormat(formatString);
+  }
+  
+  public void setFormatString(String formatString)
+  { 
+    this.formatString=formatString;
+    newFormat(formatString);
+  }
+
+  public String getFormatString()
+  { return formatString;
+  }
+  
+  private void newFormat(final String newFormatString)
+  {
+    formatLocal
+      =new ThreadLocal<SimpleDateFormat>()
+      {
+        @Override
+        public SimpleDateFormat initialValue()
+        { return new SimpleDateFormat(newFormatString);
+        }
+      };
+  }
+  
+  @Override
+  public String toString(Date val)
+  { 
+    if (val==null)
+    { return null;
+    }
+    return formatLocal.get().format(val);
+  }
+    
+
+  @Override
+  public Date fromString(String val)
+  { 
+    if (val==null)
+    { return null;
+    }
+    else
+    { 
+      try
+      { return formatLocal.get().parse(val);
+      }
+      catch (ParseException x)
+      { 
+        throw new IllegalArgumentException
+          ("Error parsing date using format '"+formatString+"': "+val,x);
+      }
+    }
+
+  }
+}
