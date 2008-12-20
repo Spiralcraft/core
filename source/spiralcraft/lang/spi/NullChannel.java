fac670a462184f104305e368ddf6a80a4420dc22
mike <>
2008-12-20 00:25:03 +0000

    Provide a useful way to bind to a terminal that doesn't provide any data.

---
+++ source/spiralcraft/lang/spi/NullChannel.java
@@ -0,0 +1,48 @@
+//
+// Copyright (c) 1998,2005 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.lang.spi;
+
+import spiralcraft.lang.Reflector;
+
+
+/**
+ * <p>A Channel which returns null output and discards all input.
+ * </p>
+ */
+public class NullChannel<T>
+  extends AbstractChannel<T>
+{
+  
+  public NullChannel(Reflector<T> reflector)
+  { super(reflector,true);
+  }
+  
+  
+  @Override
+  protected T retrieve()
+  { return null;
+  }
+  
+  @Override
+  protected boolean store(T val)
+  { return false;
+  }
+  
+  @Override
+  public boolean isWritable()
+  { return false;
+  }
+
+}
