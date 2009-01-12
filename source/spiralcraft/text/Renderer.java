702bfc62e78f57d8eea7d9864a6fa3399dd9b2a6
mike <>
2009-01-12 18:28:09 +0000

    Text Renderer interface sends arbitrary text data to a writer or String

---
+++ source/spiralcraft/text/Renderer.java
@@ -0,0 +1,51 @@
+//
+// Copyright (c) 2009,2009 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.text;
+
+import java.io.IOException;
+import java.io.Writer;
+
+
+
+/**
+ * <p>A means to render text
+ * </p>
+ * 
+ * @author mike
+ *
+ */
+public interface Renderer
+{
+
+  /**
+   * <p>Render markup to a Writer
+   * </p>
+   * 
+   * @param writer
+   * @throws IOException
+   */
+  public void render(Writer writer)
+    throws IOException;
+  
+  /**
+   * <p>Render markup to a String
+   * </p>
+   * 
+   * @return The rendered markup
+   * @throws IOException
+   */
+  public String render()
+    throws IOException;
+}
