7960b9c811c640f52f727e502cc3b6c3c5a5d206
mike <>
2009-04-12 07:42:52 +0000

    In progress work for GUI package

--- source/spiralcraft/ui/Control.java
+++ source/spiralcraft/ui/Control.java
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 1998,2005 Michael Toth
+// Copyright (c) 1998,2009 Michael Toth
 // Spiralcraft Inc., All Rights Reserved
 //
 // This package is part of the Spiralcraft project and is licensed under
@@ -17,11 +17,21 @@ package spiralcraft.ui;
 import spiralcraft.common.Lifecycle;
 
 /**
- * Base interface for UI Controls. Represents the Controller role
- *  in a Hierarchical Model/View/Controller design pattern
+ * <p>Base interface for UI Controls. Represents the Controller role
+ *   in a recursiveHierarchical Model/View/Controller design pattern. This
+ *   abstract interface provides a means to realize common UI functionality
+ *   that is independent of a given UI implementation.
+ * </p>
  *. 
- * Controls are normally peered with a single user interface component
- *   from an implementation specific toolkit (ie. Swing, WebUI, etc.)
+ * <p>Controls are normally peered with a single user interface component
+ *   from an implementation specific toolkit (ie. Swing, WebUI, etc.), and/or
+ *   a reference (in some arbitrary form) to some aspect of a model. At
+ *   the current level of abstraction, we do not assume that either the
+ *   "view" or the "model" is a Java object referencable by a bean property-
+ *   hence there is no "getter" method or type variable for either.
+ * 
+ * </p>
+ *   
  */
 public interface Control
   extends Lifecycle
