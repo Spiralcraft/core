3528cac5e71fd083f43e429f0e8d9d00e9f65219
mike <>
2009-08-11 20:46:08 +0000

    Differentiate TypeNotFoundException from errors loading a defined type.

--- source/spiralcraft/data/lang/DataTypeModel.java
+++ source/spiralcraft/data/lang/DataTypeModel.java
@@ -4,14 +4,22 @@ import java.net.URI;
 
 import spiralcraft.data.DataException;
 import spiralcraft.data.Type;
+import spiralcraft.data.TypeNotFoundException;
 import spiralcraft.data.TypeResolver;
 import spiralcraft.lang.BindException;
 import spiralcraft.lang.Reflector;
 import spiralcraft.lang.TypeModel;
+//import spiralcraft.log.ClassLog;
+// import spiralcraft.log.Level;
 
 public class DataTypeModel
   extends TypeModel
 {
+//  private static final ClassLog log
+//    =ClassLog.getInstance(DataTypeModel.class);
+//  private static final Level debugLevel
+//    =ClassLog.getInitialDebugLevel(DataTypeModel.class,null);
+  
   private static final DataTypeModel instance
     =new DataTypeModel();
   
@@ -28,9 +36,13 @@ public class DataTypeModel
     try
     { type=TypeResolver.getTypeResolver().resolve(typeURI);
     }
-    catch (DataException x)
+    catch (TypeNotFoundException x)
     {
     }
+    catch (DataException x)
+    { throw new BindException("Error resolving type "+typeURI,x);
+    }
+    
     if (type!=null)
     { return DataReflector.<X>getInstance(type);
     }
