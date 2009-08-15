362d33f58882f4374d16fc489be61b75ab72382d
mike <>
2009-08-15 04:54:11 +0000

    Added a frame type designed specifically for reading field or list values in character form.

---
+++ source/spiralcraft/data/sax/ValueFrame.java
@@ -0,0 +1,178 @@
+//
+// Copyright (c) 1998,2008 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.data.sax;
+
+import spiralcraft.data.DataException;
+import spiralcraft.data.EditableAggregate;
+import spiralcraft.data.Type;
+import spiralcraft.data.lang.DataReflector;
+import spiralcraft.data.spi.EditableArrayListAggregate;
+import spiralcraft.lang.BindException;
+import spiralcraft.lang.Channel;
+import spiralcraft.lang.Expression;
+import spiralcraft.lang.Focus;
+import spiralcraft.lang.Setter;
+import spiralcraft.lang.SimpleFocus;
+
+/**
+ * <p>Associates a typed value with an XML element, mapping and translating 
+ *   data into the spiralcraft.data Type space.
+ * </p>
+ * 
+ * 
+ * @author mike
+ *
+ */
+public class ValueFrame<T>
+  extends AbstractFrameHandler
+{
+
+  private Type<T> type;
+  
+  private Expression<EditableAggregate<T>> container;
+  private Expression<T> assignment;
+  
+  private Channel<T> channel;
+  
+  private Channel<EditableAggregate<T>> containerChannel;
+  private Channel<T> assignmentChannel;  
+  
+  public void setType(Type<T> type)
+  { this.type=type;
+  }
+  
+  public Type<?> getType()
+  { return type;
+  }
+
+
+  /**
+   * <p>An expression that references a container, such as a List or an
+   *   EditableAggregate, to which this this value will be added on
+   *   completion of the Frame
+   * </p>
+   * 
+   * @param container
+   */
+  public void setContainer(Expression<EditableAggregate<T>> container)
+  { this.container=container;
+  }
+
+  /**
+   * <p>An expression that references a field or property that will be updated
+   *   with the value on completion of the frame.
+   * </p>
+   * 
+   * @param container
+   */
+  public void setAssignment(Expression<T> assignment)
+  { this.assignment=assignment;
+  }
+  
+  @Override
+  public void bind()
+    throws BindException
+  {
+    if (type==null)
+    { 
+      throw new BindException
+        ("TupleFrameHandler for elementURI="
+        +getElementURI()+" requires a type"
+        );
+    }
+    channel=new FrameChannel<T>(DataReflector.<T>getInstance(type));
+    
+    // These must bind to the parent focus, which is our own focus
+    //   before we bind and extend the Focus chain.
+    Focus<?> parentFocus=getFocus();
+    if (container!=null)
+    { containerChannel=parentFocus.bind(container);
+    }
+    if (assignment!=null)
+    { assignmentChannel=parentFocus.bind(assignment);
+    }
+    setFocus(new SimpleFocus<T>(parentFocus,channel));
+    setTextBinding(Expression.create("."));
+    super.bind();
+  }
+  
+  @Override
+  protected void openData()
+    throws DataException
+  {
+
+  }
+  
+  @Override
+  protected void closeData()
+    throws DataException
+  {     
+
+    Setter.applyArrayIfNull(defaultSetters);
+    
+    if (assignmentChannel!=null)
+    { assignmentChannel.set(channel.get());
+    }
+    
+    if (containerChannel!=null)
+    { 
+      EditableAggregate<T> container
+        =containerChannel.get();
+      
+      T value=channel.get();
+      
+      if (value!=null)
+      {
+        if (container==null)
+        { 
+          if (!containerChannel.isWritable())
+          { 
+            throw new DataException
+              ("Container for "
+              +type.getURI()
+              +" does not exist and could not be created because "
+              +" the container channel is not writable." 
+              );
+          }
+          
+          container
+            =new EditableArrayListAggregate<T>
+              (Type.getAggregateType(type));
+          
+          if (!containerChannel.set(container))
+          {
+            throw new DataException
+              ("Container for "
+              +type.getURI()
+              +" does not exist and could not be created because "
+              +" the container channel did not accept the new Container. "
+              );
+          }
+        }
+        container.add(value);
+      }
+    }
+
+    if (debug)
+    { 
+      Object val=channel.get();
+      log.fine("elementURI="+getElementURI()+": "+(val!=null?val:""));
+    }
+
+  }
+  
+  
+  
+}
