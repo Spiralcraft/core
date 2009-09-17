578e642c93202eaebb2b4be24fceea82403cb828
mike <>
2009-09-17 00:12:46 +0000

    Enhanced debugging instrumentation, particularly around getOutputStream Provide optional inputBufferLength property to buffer provided inputStreams when set value >0.

--- source/spiralcraft/vfs/url/URLResource.java
+++ source/spiralcraft/vfs/url/URLResource.java
@@ -14,15 +14,22 @@
 //
 package spiralcraft.vfs.url;
 
+import spiralcraft.io.DebugInputStream;
+import spiralcraft.io.InputStreamWrapper;
 import spiralcraft.log.ClassLog;
+import spiralcraft.log.Level;
 import spiralcraft.vfs.AbstractResource;
 import spiralcraft.vfs.UnresolvableURIException;
 
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.MalformedURLException;
+import java.util.List;
+import java.util.Map;
 
+import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -33,10 +40,14 @@ public class URLResource
   private static final ClassLog log
     =ClassLog.getInstance(URLResource.class);
   
-  private static final boolean debug=false;
+  private static final Level debugLevel
+    =ClassLog.getInitialDebugLevel(URLResource.class,null);
+  
   private int timeoutMS=-1;
+  
     
   private final URL _url;
+  private int inputBufferLength;
  
 
   public URLResource(URL url)
@@ -61,6 +72,10 @@ public class URLResource
     }
   }
 
+  public void setInputBufferLength(int inputBufferLength)
+  { this.inputBufferLength=inputBufferLength;
+  }
+  
   /**
    * Specify the default timeout in milleseconds for IO operations. A value
    *   of -1 indicates that the platform defaults for URLConnection should be
@@ -76,12 +91,62 @@ public class URLResource
   public InputStream getInputStream()
     throws IOException
   { 
-    URLConnection connection=_url.openConnection();
+    final URLConnection connection=_url.openConnection();
     setupConnection(connection);
     connection.setDoInput(true);
-    return connection.getInputStream();
+    connection.connect();
+    if (debugLevel.canLog(Level.DEBUG))
+    { dumpHeaders("On getInputStream()",connection.getHeaderFields());
+    }
+    if (connection instanceof HttpURLConnection)
+    {
+      final HttpURLConnection httpConnection=(HttpURLConnection) connection;
+      InputStream source=httpConnection.getInputStream();
+
+      if (debugLevel.canLog(Level.FINE))
+      { source=new DebugInputStream(source);
+      }
+      
+      if (inputBufferLength>0)
+      { 
+        if (debugLevel.canLog(Level.DEBUG))
+        { log.debug("inputBufferLength="+inputBufferLength);
+        }
+        source=new BufferedInputStream(source,inputBufferLength);
+      }
+   
+          
+      return new InputStreamWrapper(source)
+      {        
+        @Override
+        public void close()
+          throws IOException
+        {
+          
+          super.close();
+          httpConnection.disconnect();
+          if (debugLevel.canLog(Level.DEBUG))
+          { log.fine("Disconnected "+_url);
+          }
+        }
+      };
+      
+    }
+    else
+    { return connection.getInputStream();      
+    }
   }
 
+  private void dumpHeaders(String message,Map<String,List<String>> headers)
+  {
+    StringBuffer headerBuf=new StringBuffer();
+    for (Map.Entry<String,List<String>> entry : headers.entrySet())
+    { 
+      headerBuf.append("\r\n"+entry.getKey()+"="+entry.getValue());
+    }
+    log.fine(message+": Response headers for "+_url+":"+headerBuf.toString());
+  }
+  
   private void setupConnection(URLConnection connection)
   {
     if (timeoutMS>-1)
@@ -91,7 +156,13 @@ public class URLResource
     }
     connection.setUseCaches(false);
     connection.setAllowUserInteraction(false);
-    connection.addRequestProperty("Connection","close");
+    connection.setRequestProperty("Connection","close");
+//    if (connection instanceof HttpURLConnection)
+//    {
+//      HttpURLConnection httpConnection=(HttpURLConnection) connection;
+//      
+//    }
+    
   }
   
   @Override
@@ -106,6 +177,10 @@ public class URLResource
     URLConnection connection=_url.openConnection();
     setupConnection(connection);
     connection.setDoOutput(true);
+    connection.connect();
+    if (debugLevel.canLog(Level.DEBUG))
+    { dumpHeaders("On getOutputStream()",connection.getHeaderFields());
+    }
     return connection.getOutputStream();
   }
 
@@ -123,8 +198,8 @@ public class URLResource
     connection.setDoInput(true);
     connection.setDoOutput(false);
     connection.connect();
-    if (debug)
-    { log.fine(connection.getContentType());
+    if (debugLevel.canLog(Level.DEBUG))
+    { dumpHeaders("On exists()",connection.getHeaderFields());
     }
     return connection.getDate()>0;    
   }
@@ -135,10 +210,14 @@ public class URLResource
   {
     URLConnection connection=_url.openConnection();
     setupConnection(connection);
-    connection.setDoInput(true);
+    connection.setDoInput(false);
     connection.setDoOutput(false);
     connection.connect();
+    if (debugLevel.canLog(Level.DEBUG))
+    { dumpHeaders("On getSize()",connection.getHeaderFields());
+    }
     return connection.getContentLength();
+
     
   }
   
