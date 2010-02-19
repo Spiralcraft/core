4d1671d0b5b61eb9439b2599ef421ff0c7657562
mike <>
2010-02-19 08:49:21 +0000

    Type coercion introduced into Logical expressions to             permit some number comparison operations (ie. float to Double)             by using Number class conversions. Factored Translator interface             for logical expressions (LogicalNode) into inner class so             implementation can be chosen at bind-time.

---
+++ source/spiralcraft/lang/spi/Coercion.java
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2010 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.lang.spi;
+
+/**
+ * <p>Coerces an input type to an output type. May lose precision in
+ *   the process
+ * </p>
+ * 
+ * @author mike
+ *
+ * @param <Tinput>
+ * @param <Touput>
+ */
+public interface Coercion<Tinput,Toutput>
+{
+  Toutput coerce(Tinput input);
+}
