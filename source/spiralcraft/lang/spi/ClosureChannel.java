b7eb3bfdf9bc2fd0e50f54754e9bbe6cb1a58490
mike <>
2010-03-31 09:11:09 +0000

    Moved to lang/spi

---
+++ source/spiralcraft/lang/spi/ClosureChannel.java
@@ -0,0 +1,59 @@
+//
+// Copyright (c) 2010 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.lang.spi;
+
+import spiralcraft.lang.AccessException;
+import spiralcraft.lang.Channel;
+
+public class ClosureChannel<T>
+  extends AbstractChannel<T>
+{
+  private final ClosureFocus<?> closure;
+  private final Channel<T> enclosedChannel;
+  
+  public ClosureChannel(ClosureFocus<?> closure,Channel<T> enclosedChannel)
+  { 
+    super(enclosedChannel.getReflector());
+    this.enclosedChannel=enclosedChannel;
+    this.closure=closure;
+  }
+  
+  @Override
+  protected T retrieve()
+  {
+    closure.push();
+    try
+    { return enclosedChannel.get();
+    }
+    finally
+    { closure.pop();
+    }
+  }
+
+  @Override
+  protected boolean store(
+    T val)
+    throws AccessException
+  {
+    closure.push();
+    try
+    { return enclosedChannel.set(val);
+    }
+    finally
+    { closure.pop();
+    }
+  }
+
+}
