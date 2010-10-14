9c1cf7ba56131b6071d68013e6aa540fab1f54ed
mike <>
2010-10-14 21:07:58 +0000

    Instant is immutable

--- source/spiralcraft/time/Instant.java
+++ source/spiralcraft/time/Instant.java
@@ -16,6 +16,8 @@ package spiralcraft.time;
 
 import java.math.BigDecimal;
 
+import spiralcraft.common.Immutable;
+
 
 /**
  * <p>A specific point in time relative to the Posix EPOCH of 
@@ -32,6 +34,7 @@ import java.math.BigDecimal;
  * @author mike
  *
  */
+@Immutable
 public class Instant
   implements Comparable<Instant>
 {
