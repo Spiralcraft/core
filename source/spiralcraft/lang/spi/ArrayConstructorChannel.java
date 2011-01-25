5264822a4623c5cce5abbc9b2e450f78036df835
mike <>
2011-01-25 08:55:48 +0000

    doc

--- source/spiralcraft/lang/spi/ArrayConstructorChannel.java
+++ source/spiralcraft/lang/spi/ArrayConstructorChannel.java
@@ -1,6 +1,17 @@
-/**
- * Construct an Array from a source that is Iterable
- */
+//
+// Copyright (c) 1998,2010 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
 package spiralcraft.lang.spi;
 
 import java.lang.reflect.Array;
@@ -15,6 +26,9 @@ import spiralcraft.lang.IterationDecorator;
 import spiralcraft.lang.Reflector;
 import spiralcraft.lang.reflect.ArrayReflector;
 
+/**
+ * Construct an Array from a source that is Iterable
+ */
 public class ArrayConstructorChannel<C,T>
   extends SourcedChannel<C,T[]>
 {
