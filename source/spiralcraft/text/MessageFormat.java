de6acf1a9840dc4f817e4838cad9d7c403b5ed6e
mike <>
2011-02-14 17:25:08 +0000

    MessageFormat provides a mechanism for templating             string resources used for labels, messages, tool-tips, etc. while             providing for the level of indirection required for I18N support.             Future versions of this object will support ResourceBundle based             resolution of templates based on language and locale.

---
+++ source/spiralcraft/text/MessageFormat.java
@@ -0,0 +1,173 @@
+//
+// Copyright (c) 2011 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.text;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.LinkedList;
+import java.util.List;
+
+import spiralcraft.lang.BindException;
+import spiralcraft.lang.Binding;
+import spiralcraft.lang.Contextual;
+import spiralcraft.lang.Focus;
+import spiralcraft.lang.ParseException;
+import spiralcraft.text.markup.MarkupException;
+import spiralcraft.text.markup.MarkupHandler;
+import spiralcraft.text.markup.MarkupParser;
+
+/**
+ * <p>Generates a message from a basic markup template syntax.
+ * </p>
+ * 
+ * <p>This is a placeholder to support future I18N by providing a
+ *   ResourceBundle for the templates
+ * </p>
+ * 
+ * @author mike
+ *
+ */
+public class MessageFormat
+  implements Contextual,Renderer
+{
+  private static final MarkupParser substitutionParser
+    =new MarkupParser("{|","|}",'\\');
+  
+  private List<AbstractRenderer> renderers
+    =new LinkedList<AbstractRenderer>();
+
+  public MessageFormat(String template)
+    throws spiralcraft.text.ParseException,MarkupException
+  {
+    substitutionParser.parse
+      (template
+      ,new MarkupHandler()
+      {
+        private ParsePosition position;
+        
+        @Override
+        public void handleContent(
+          CharSequence text)
+        { renderers.add(new ContentRenderer(text));
+        }
+
+        @Override
+        public void handleMarkup(
+          CharSequence code)
+          throws spiralcraft.text.ParseException
+        { 
+          try
+          { renderers.add(new MarkupRenderer(code));
+          }
+          catch (ParseException x)
+          { throw new spiralcraft.text.ParseException
+              ("Error parsing expression '"+code+"'",position,x);
+          }
+        }
+
+        @Override
+        public void setPosition(ParsePosition position)
+        { this.position=position;
+          
+        } 
+      }
+      ,null
+      );
+  }
+  
+  @Override
+  public void render(Writer writer)
+    throws IOException
+  {
+    for (Renderer renderer:renderers)
+    { renderer.render(writer);
+    }
+  }
+
+  @Override
+  public Focus<?> bind(Focus<?> focusChain)
+    throws BindException
+  {
+    for (AbstractRenderer renderer:renderers)
+    { renderer.bind(focusChain);
+    }
+    return focusChain;
+  }
+
+}
+
+abstract class AbstractRenderer
+  implements Renderer,Contextual
+{
+ 
+}
+
+class ContentRenderer
+  extends AbstractRenderer
+{
+  
+  private CharSequence content;
+  
+  public ContentRenderer(CharSequence content)
+  { this.content=content;
+  }
+  
+  @Override
+  public void render(
+    Writer writer)
+    throws IOException
+  { writer.append(content);
+  }
+
+  @Override
+  public Focus<?> bind(
+    Focus<?> focusChain)
+    throws BindException
+  { return focusChain;
+  }
+  
+}
+
+class MarkupRenderer
+  extends AbstractRenderer
+{
+  
+  private Binding<?> binding;
+  
+  public MarkupRenderer(CharSequence expression) 
+    throws ParseException
+  { this.binding=new Binding<Object>(expression.toString());
+  }
+  
+  @Override
+  public void render(Writer writer)
+    throws IOException
+  { 
+    Object ret=binding.get();
+    if (ret!=null)
+    { writer.write(ret.toString());
+    }
+  }
+  
+  @Override
+  public Focus<?> bind(
+    Focus<?> focusChain)
+    throws BindException
+  { 
+    binding.bind(focusChain);
+    return focusChain;
+  }
+
+}
