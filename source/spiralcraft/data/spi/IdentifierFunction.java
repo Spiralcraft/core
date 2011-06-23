5b5b6bec75737af736520a504d1f08af844dacd3
mike <>
2011-06-23 23:07:54 +0000

    Identifiers should always reference the master type             (archetype) so buffers and deltas and standard tuples for the             same entity can be associated

--- source/spiralcraft/data/spi/IdentifierFunction.java
+++ source/spiralcraft/data/spi/IdentifierFunction.java
@@ -5,12 +5,15 @@ import spiralcraft.data.KeyTuple;
 import spiralcraft.data.Projection;
 import spiralcraft.data.Tuple;
 import spiralcraft.data.Type;
+import spiralcraft.data.core.DeltaType;
+import spiralcraft.data.session.BufferType;
 import spiralcraft.lang.BindException;
 
 public class IdentifierFunction<T>
   extends DataKeyFunction<KeyTuple,T>
 {
   private final Type<T> masterType;
+  private final Type<T> identifiedType;
   
   @SuppressWarnings("unchecked")
   public IdentifierFunction(Projection<T> projection)
@@ -18,12 +21,19 @@ public class IdentifierFunction<T>
   { 
     super(projection);
     this.masterType=(Type<T>) projection.getSource().getType();
-    
+    if (masterType instanceof BufferType
+        || masterType instanceof DeltaType
+        )
+    { identifiedType=(Type<T>) masterType.getArchetype();
+    }
+    else
+    { identifiedType=masterType;
+    }
   }  
  
   @Override
   public KeyIdentifier<T> createKeyTuple(Tuple projectionValue)
     throws DataException
-  { return new KeyIdentifier<T>(masterType,projectionValue);
+  { return new KeyIdentifier<T>(identifiedType,projectionValue);
   }
 }
