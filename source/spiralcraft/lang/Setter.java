0e54e577823038f4ce8afe8333a4fea2205be6a0
mike <>
2011-08-22 03:41:51 +0000

    Setter now respects numeric coercion rules

--- source/spiralcraft/lang/Setter.java
+++ source/spiralcraft/lang/Setter.java
@@ -1,7 +1,22 @@
+//
+// Copyright (c) 2007,2011 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
 package spiralcraft.lang;
 
-
+import spiralcraft.lang.kit.CoercionChannel;
 import spiralcraft.log.ClassLog;
+import spiralcraft.util.lang.NumericCoercion;
 
 /**
  * Copies the value of a source channel to a target
@@ -39,6 +54,7 @@ public class Setter<T>
   private Channel<T> target;
   private boolean debug;
   
+  @SuppressWarnings({ "rawtypes", "unchecked" })
   public Setter(Channel<? extends T> source,Channel<T> target)
     throws BindException
   { 
@@ -46,10 +62,27 @@ public class Setter<T>
     this.target=target;
     if (target!=null && !target.getReflector().isAssignableFrom(source.getReflector()))
     { 
-      throw new BindException
-        ("Cannot assign a "+source.getReflector().getTypeURI()
-        +" to a location of type "+target.getReflector().getTypeURI()
-        );
+      NumericCoercion coercion
+        =NumericCoercion.instance(target.getContentType());
+  
+      if (coercion==null)
+      {
+      
+        throw new BindException
+          ("Cannot assign a "+source.getReflector().getTypeURI()
+          +" to a location of type "+target.getReflector().getTypeURI()
+          );
+      }
+      else
+      { 
+        this.source
+          =new CoercionChannel
+            (target.getReflector()
+            ,source
+            ,coercion
+            );
+      }
+
     }
   }
   
