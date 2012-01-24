a502e3452b9984b00fd4b2130f1bceba5184bec5
mike <>
2012-01-24 05:16:17 +0000

    Javadoc

--- source/spiralcraft/lang/spi/BindingChannel.java
+++ source/spiralcraft/lang/spi/BindingChannel.java
@@ -1,3 +1,17 @@
+//
+// Copyright (c) 2011,2012 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
 package spiralcraft.lang.spi;
 
 import spiralcraft.lang.AccessException;
@@ -9,6 +23,17 @@ import spiralcraft.lang.Focus;
 import spiralcraft.lang.Reflector;
 import spiralcraft.log.ClassLog;
 
+/**
+ * <p>Assigns the value of an expression bound to a "source" context
+ *   to a location in a "target" context. 
+ * </p>
+ * 
+ * <p>This is usually expressed in the form "target := source", where the
+ *   ":=" operator signifies "binding" 
+ * </p>
+ *
+ * @param <T>
+ */
 public class BindingChannel<T>
   extends SourcedChannel<T,T>
 {
@@ -191,13 +216,6 @@ public class BindingChannel<T>
   throws AccessException
   { 
     assertTarget();
-    if (targetChannel==null)
-    { 
-      throw new IllegalStateException
-        ("Target '"+targetX+"' not bound. The ':=' operator may not be used"
-        +" in this scenario"
-        );
-    }
     boolean set=targetChannel.set(val);
     source.set(val);
     return set;
