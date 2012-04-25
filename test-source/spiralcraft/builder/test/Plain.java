bf4d9c715e55901ad123bc2626720000578da38b
mike <>
2012-04-25 02:44:14 +0000

    0.4.17-dev5

---
+++ test-source/spiralcraft/builder/test/Plain.java
@@ -0,0 +1,19 @@
+package spiralcraft.builder.test;
+
+
+public class Plain
+{
+  private String title;
+
+  public Plain()
+  {
+  }
+  
+  public void setTitle(String title)
+  { this.title=title;
+  }
+  
+  public String getTitle()
+  { return title;
+  }
+}
