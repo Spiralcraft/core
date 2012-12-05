a2663721e8db5ae7b1576ef933630a2d7bdc41b5
mike <>
2012-12-05 22:34:29 +0000

    0.4.17-dev32

---
+++ test-source/spiralcraft/lang/test/Bar.java
@@ -0,0 +1,19 @@
+package spiralcraft.lang.test;
+
+public class Bar
+{
+
+  public String publicField="default";
+  
+  public String[] stringArray
+    ={"one","two","three"};
+  
+  public String getTheField()
+  { return publicField;
+  }
+
+  public String[] getTheArray()
+  { return stringArray;
+  }
+
+}
