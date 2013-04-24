633ba28510e5d765db71d6a92a91ac6a04016454
mike <>
2013-04-24 17:51:50 +0000

    Refactor RandomUtil->Random to provide for pluggable             random number generator and default secure and standard             generators.

--- source/spiralcraft/util/RandomUtil.java
+++ source/spiralcraft/util/RandomUtil.java
@@ -1,6 +1,5 @@
 package spiralcraft.util;
 
-import java.util.Random;
 
 /**
  * Utility class to support the generation of random numbers and Strings.
@@ -10,53 +9,10 @@ import java.util.Random;
  */
 public class RandomUtil
 {
-  private static char[] chars=
-    {'A'
-    ,'B'
-    ,'C'
-    ,'D'
-    ,'E'
-    ,'F'
-    ,'G'
-    ,'H'
-    ,'I'
-    ,'J'
-    ,'K'
-    ,'L'
-    ,'M'
-    ,'N'
-    ,'O'
-    ,'P'
-    ,'Q'
-    ,'R'
-    ,'S'
-    ,'T'
-    ,'U'
-    ,'V'
-    ,'W'
-    ,'X'
-    ,'Y'
-    ,'Z'
-    ,'0'
-    ,'1'
-    ,'2'
-    ,'3'
-    ,'4'
-    ,'5'
-    ,'6'
-    ,'7'
-    ,'8'
-    ,'9'
-    };
-
-  private static final Random RANDOM=new Random();
 
   public static String generateString(int length)
-  {
-    StringBuilder out=new StringBuilder(length);
-    for (int i=0;i<length;i++)
-    { out.append(chars[ Math.abs(RANDOM.nextInt()%36) ]);
-    }
-    return out.toString();
+  { return Random.secureInstance().generateString(length);
   }
+
+  
 }
