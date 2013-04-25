908dbdb526926f5f940a239a497a98a92286a1ef
mike <>
2013-04-25 02:46:04 +0000

    Make sure that spiralcraft.lang reflection types can be             found even when aliased through "context" URIs that resolve             through VFS Packages.

--- source/spiralcraft/lang/reflect/BeanTypeModel.java
+++ source/spiralcraft/lang/reflect/BeanTypeModel.java
@@ -1,11 +1,15 @@
 package spiralcraft.lang.reflect;
 
+import java.io.IOException;
 import java.net.URI;
 
 import spiralcraft.beans.BeanInfoCache;
 import spiralcraft.lang.BindException;
 import spiralcraft.lang.Reflector;
 import spiralcraft.lang.TypeModel;
+import spiralcraft.util.URIUtil;
+import spiralcraft.vfs.Package;
+import spiralcraft.vfs.Resource;
 
 public class BeanTypeModel 
   extends TypeModel
@@ -21,6 +25,27 @@ public class BeanTypeModel
   public <X> Reflector<X> findType(URI typeURI)
     throws BindException
   { 
+    try
+    {
+      if (typeURI.getScheme()!=null && typeURI.getScheme().equals("context"))
+      { 
+        Resource classResource=Package.findResource(typeURI+".class");
+        if (classResource!=null)
+        {
+        
+          if (classResource.getURI().getScheme().equals("class"))
+          { typeURI=URIUtil.removePathSuffix(classResource.getURI(),".class");
+          }
+        }
+        else
+        { return null;
+        }
+      }
+    }
+    catch (IOException x)
+    { throw new BindException("Error resolving context URI "+typeURI);
+    }
+    
     Class<?> clazz=BeanInfoCache.<X>getClassForURI(typeURI);
     if (clazz!=null)
     { return BeanReflector.getInstance(clazz);
