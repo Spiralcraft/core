de94289dad8f372716dab46f58f6f95b15a55609
Mike Toth <mike.git@spiralcraft.com>
2014-04-08 19:05:16 -0700

    Make sure that local key definitions in override/hide             keys defined in archetypes.

--- source/spiralcraft/data/Type.java
+++ source/spiralcraft/data/Type.java
@@ -474,11 +474,11 @@ public abstract class Type<T>
     if (getBaseType()!=null)
     { key=getBaseType().findKey(fieldNames);
     }
-    if (key==null && getArchetype()!=null)
-    { key=(Key<T>) getArchetype().findKey(fieldNames);
-    }
     if (key==null)
     { key=(Key<T>) getScheme().findKey(fieldNames);
+    }
+    if (key==null && getArchetype()!=null)
+    { key=(Key<T>) getArchetype().findKey(fieldNames);
     }
     return key;
   }
